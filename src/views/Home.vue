32085bd51c996b0172cc1032db8d0d1b9331d847
Benjamin <benjamin.karlsson@squeed.com>
2020-09-04 14:14:43 +0200

    updating about source code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,18 +5,23 @@
       src="https://github.com/b3kay.png"
       class="m-auto mb-4"
     >
-    <HelloWorld msg="This is a Sandbox for my personal Vue.js Training." />
+    <h1>This is a Sandbox for my personal Vue.js Training."</h1>
+    <p>- Benjamin Karlsson</p>
+
+    <p>
+      The source code to the project can be found at
+      <a
+        href="https://github.com/B3Kay/quiz_app"
+        alt="link to source"
+      >github</a>
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
-  },
 };
 </script>
